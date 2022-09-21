c8225b980ca612ca45274d7e49fc92550b6bffe9
Buddyboy2007 <buddyboy2007@gmail.com>
2022-09-21 17:49:36 -0400

    text adventure

---
+++ 1.4.3 Text adventure/main.js
@@ -0,0 +1,32 @@
+/*
+Enter the name of your game into the string assigned to the gameName variable. 
+Enter your name and the name of any collaborators into the string assigned to the creator variable. 
+*/
+let gameName = "";
+let creator = "";
+
+let gameNameContainer = document.getElementById("gameNameContainer")
+gameNameContainer.textContent += gameName;
+
+let NameContainer = document.getElementById("NameContainer")
+NameContainer.textContent += creator;
+
+/*----------
+Create a new variable that will contain a new instance of the Text Game Engine. 
+*/
+
+
+/* ----------
+Create a function named Start that will run when the player starts the game.
+*/
+
+
+/* ----------
+Add to the game by creating new Scenes. 
+*/
+
+
+/* ----------
+DO NOT REMOVE OR EDIT ANY CODE BELOW THIS COMMENT! The game is ran by calling the Start function. 
+*/
+Start();
